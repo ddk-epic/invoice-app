12a7cce9b1460a967c1829bdae5e68efc334987d
ddk <dattimio95@gmail.com>
2025-06-20 17:07:22 +0200

    add clerk publishable key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
@@ -33,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       afterSignOutUrl={"/sign-in"}
       appearance={{
         elements: {
