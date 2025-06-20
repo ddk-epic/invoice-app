0f15e771a74950d91f4152b87606ab2c7024135e
ddk <dattimio95@gmail.com>
2025-06-21 00:00:04 +0200

    fix GET request

--- src/app/invoice/[invoiceId]/pdf/route.tsx
+++ src/app/invoice/[invoiceId]/pdf/route.tsx
@@ -1,8 +1,8 @@
 export async function GET(
   request: Request,
-  { params }: { params: { invoiceId: string } }
+  { params }: { params: Promise<{ invoiceId: string }> }
 ) {
-  const { invoiceId } = params;
+  const { invoiceId } = await params;
 
   return Response.json({
     message: `Generating PDF for invoice ${invoiceId}`,
