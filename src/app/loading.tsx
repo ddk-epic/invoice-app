c34749e93e63405d4e3e5d76c6dcaae0982c3ffc
ddk <dattimio95@gmail.com>
2025-06-29 01:57:00 +0200

    add loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <main className="wrapper top flex justify-center items-center min-h-screen w-full overflow-y-scroll">
+      <div className="text-4xl text-gray-400">Loading...</div>
+    </main>
+  );
+}
+
+export default Loading;
