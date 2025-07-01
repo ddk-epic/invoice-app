adb6f0a9ee7b9562991e681b28a11a3a30bc8c81
ddk <dattimio95@gmail.com>
2025-07-01 22:37:26 +0200

    fix invoice table

--- src/app/invoice/page.tsx
+++ src/app/invoice/page.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { Badge } from "@/components/ui/badge";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import {
   Table,
   TableBody,
@@ -7,11 +10,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Badge } from "@/components/ui/badge";
-import { sampleRecentInvoices, getStatusColor } from "@/constants/constants";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 
-function InvoiceViewAll() {
+import { InvoiceData } from "@/constants/types";
+import { getStatusColor } from "@/constants/constants";
+
+import { QUERIES } from "@/server/db/queries";
+
+async function InvoiceViewAll() {
+  const invoiceList = (await QUERIES.getAllInvoices()) as InvoiceData[];
   return (
     <main className="wrapper top min-h-screen bg-gray-50">
       <div className="px-4 py-6 sm:px-0">
@@ -35,18 +41,20 @@ function InvoiceViewAll() {
                 </TableRow>
               </TableHeader>
               <TableBody>
-                {sampleRecentInvoices.map((invoice) => (
+                {invoiceList.map((invoice) => (
                   <TableRow key={invoice.id}>
-                    <TableCell className="font-medium">{invoice.id}</TableCell>
-                    <TableCell>{invoice.client}</TableCell>
-                    <TableCell>{invoice.amount}</TableCell>
+                    <TableCell className="font-medium">
+                      {invoice.invoiceId}
+                    </TableCell>
+                    <TableCell>{invoice.sendTo.name}</TableCell>
+                    <TableCell>{invoice.total}</TableCell>
                     <TableCell>
                       <Badge className={getStatusColor(invoice.status)}>
                         {invoice.status.charAt(0).toUpperCase() +
                           invoice.status.slice(1)}
                       </Badge>
                     </TableCell>
-                    <TableCell>{invoice.date}</TableCell>
+                    <TableCell>{invoice.createdAt.toDateString()}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
