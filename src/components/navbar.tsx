e737623f24f2a297301a613be39fe90352837d48
ddk <dattimio95@gmail.com>
2025-07-09 00:15:18 +0200

    add return link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { FileText } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
 import {
   SignedOut,
   SignInButton,
@@ -8,17 +9,24 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 
+import Link from "next/link";
+
 function Navbar() {
   return (
     <nav className="fixed w-full bg-white border-b border-gray-200">
       <div className="wrapper flex justify-between h-16">
         <div className="flex items-center">
-          <div className="flex-shrink-0 flex items-center">
-            <FileText className="h-8 w-8 text-purple-600" />
-            <span className="ml-2 text-xl font-bold text-gray-900">
-              Invoice App
-            </span>
-          </div>
+          {/* Link */}
+          <Link href="/dashboard">
+            <div className="flex-shrink-0 flex items-center">
+              <FileText className="h-8 w-8 text-purple-600" />
+              <div>
+                <span className="ml-2 text-xl font-bold text-gray-900">
+                  Invoice App
+                </span>
+              </div>
+            </div>
+          </Link>
         </div>
 
         <div className="flex items-center space-x-1">
